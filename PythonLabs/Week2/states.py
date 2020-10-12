069c95ecf03b015f7fba27ae26ff454901035cb1
s02152sa <sulaiman.alhmoudi@student.manchester.ac.uk>
2020-10-12 16:42:21 +0400

    source code for traffic light states

---
+++ PythonLabs/Week2/states.py
@@ -0,0 +1,8 @@
+def printState():
+ print ("Red Light is " + redLight)
+ print ("Yellow Light is " + yellowLight)
+ print ("Green Light is " + greenLight)
+redLight = "on"
+yellowLight = "off"
+greenLight = "off"
+printState()
