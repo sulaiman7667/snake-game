232db65e16129469c589f1124cf0216edd09de36
s02152sa <sulaiman.alhmoudi@student.manchester.ac.uk>
2020-10-23 15:25:29 +0400

    added spellchecker for file and optimized code

--- coursework_01/coursework1.py
+++ coursework_01/coursework1.py
@@ -1,56 +1,107 @@
+import difflib
+
+with open("englishwords.txt", "r") as f:
+	wordlist = f.read()
 
 
-selectedchoice = input("enter 1 to spell check a sentence. enter 2 to spell check a file: (enter 0 to quit.) ")
+	
+
+#####################################################
+while True:
+	selectedchoice = input("enter 1 to spell check a sentence. enter 2 to spell check a file: (enter 0 to quit.) ")
+	if (int(selectedchoice) < 3):
+		break
+	print("you entered an invalid input. please try again.")	
 
 
-if (int(selectedchoice) > 2):
-	print("you entered an invalid input. please try again.")
 
-elif (int(selectedchoice) == 1):
-		sentence = input("type in your sentence: ")
-		words_in_sentence = []
-		y = 0
-		z = 0
-		a = 0
-		for x in range (0, len(sentence)):
-	      
-		   if(sentence[x] == " "):
-		   
-		      words_in_sentence.insert(z, sentence[a:x])
-		      y = len(words_in_sentence[z])
-		      z = z+1
-		      a = 0
-		      for word in words_in_sentence:
-		       a+=len(word) + 1
-		words_in_sentence.insert(z, sentence[a:len(sentence)])
-
+if (int(selectedchoice) == 1):
+	sentence = input("type in your sentence: ")
+	errors = 0
+	errorslist = []
+	for word in sentence.casefold().split():
+		if (word not in wordlist):
+			errors = errors + 1
+			errorslist.append(word)
+	if(errors > 0):
+		option = input("select an option:" + "\n1. Ignore error." + "\n2. Find error." + "\n3. Add word to dictionary." + "\n4. Find possible suggestions.")
 		
-		import sys
+		if (int(option) == 1):
+				quit()
 
-		with open("englishwords.txt", "r") as f:
-			wordlist = f.read()
-			for f in range(0, len(words_in_sentence)):
-				if (words_in_sentence[f] not in wordlist):
+		elif (int(option) == 2):
+			for i in range (0, len(errorslist)):
+				print("?" + errorslist[i] + "?")
+			
 
-					print("false")
-		print(words_in_sentence)
-		
+
+
+
+		elif(int(option) == 3):
+				f = open("englishwords.txt", "a")
+				for g in range (0, len(errorslist)):
+			 		f.write("\n" + errorslist[g])
+
+		elif (int(option) == 4):
+			for g in range (0, len(errorslist)):
+				suggestion = difflib.get_close_matches(errorslist[g], wordlist)
+				print(f' did you mean {", ".join(str(x) for x in suggestion)} instead of lol')
+
+				 		
+
+
 		
 				
 
 
 
-elif (int(selectedchoice) == 2):
+if (int(selectedchoice) == 2):
 	file = input("enter your file name: ")
+	with open(file, "r") as f:
+		textfile = f.read()
+	errors = 0
+	errorslist = []
+	for word in textfile.casefold().split():
+		if (word not in wordlist):
+			errors = errors + 1
+			errorslist.append(word)
+	if(errors > 0):
+		option = input("select an option:" + "\n1. Ignore error." + "\n2. Find error." + "\n3. Add word to dictionary." + "\n4. Find possible suggestions.")
+		
+		if (int(option) == 1):
+				quit()
+
+		elif (int(option) == 2):
+			for i in range (0, len(errorslist)):
+				print("?" + errorslist[i] + "?")
+			
 
 
 
-	
+
+		elif(int(option) == 3):
+				f = open("englishwords.txt", "a")
+				for g in range (0, len(errorslist)):
+			 		f.write("\n" + errorslist[g])
+
+		elif (int(option) == 4):
+			for g in range (0, len(errorslist)):
+				suggestion = difflib.get_close_matches(errorslist[g], wordlist)
+				print(f' did you mean {", ".join(str(x) for x in suggestion)} instead of lol')	
 
 
 
 
 
 
-	
+
+
+
+
+
+
+
+
+
+
 
