5057dbd80a7e9bd0cddfdb402562380a3bd57a00
s02152sa <sulaiman.alhmoudi@student.manchester.ac.uk>
2020-11-20 22:57:44 +0400

    moving snake

--- Coursework_02/Coursework_02.py
+++ Coursework_02/Coursework_02.py
@@ -14,6 +14,7 @@ food_x = [200]
 food_y = [200]
 score = 0
 highscore = 0
+direction = "Right"
 def import_images():
     global snake_img, food_img
     snake_img = PhotoImage(file = "snake.png")
@@ -29,9 +30,24 @@ def create_objects():
     c.create_image(food_x[0], food_y[0], image = food_img)
 
 def move_snake():
-    global snake_x
-    for i in range (len(snake_x)):
-        snake_x[i] = snake_x[i] + 20
+    global snake_x, snake_y
+    if(direction == "Right"):
+        new_x = snake_x[0] + 20
+        new_y = snake_y[0]
+    if(direction == "Left"):
+        new_x = snake_x[0] - 20
+        new_y = snake_y[0]
+    if(direction == "Up"):
+        new_x = snake_x[0]
+        new_y = snake_y[0] - 20
+    if(direction == "Down"):
+        new_x = snake_x[0]
+        new_y = snake_y[0] + 20
+        
+    snake_x = [new_x] + snake_x[:-1]
+    snake_y = [new_y] + snake_y[:-1]
+    
+
     for tag, x, y in zip(c.find_withtag("snake_block"), snake_x, snake_y):
         c.coords(tag, x, y)
 
@@ -47,6 +63,16 @@ def check_snake_collision():
         quit()
     if((snake_x[0] in snake_x[1:]) and (snake_y[0] in snake_y[1:])):
         quit()
+def update_direction(key):
+    global direction
+    temp_direction = key.keysym
+    directions = ["Up", "Right", "Left", "Down"]
+    opp_directions1 = [["Up", "Down"], ["Left", "Right"]]
+    opp_directions2 = [["Down", "Up"], ["Right", "Left"]]
+    if(temp_direction in directions and [temp_direction, direction] not in opp_directions1 and [temp_direction, direction] not in opp_directions2):
+        direction = temp_direction
+    
+
 
 def perform_all_functions():
     import_images()
@@ -66,14 +92,11 @@ perform_all_functions()
 
 
 
-
+c.bind_all("<Key>", update_direction)
 
 
   
 
 
 
-
-
-     
 window.mainloop()
